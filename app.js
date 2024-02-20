a7c9b6357489fa9cd5fd7efa28c5e3f27c62af81
Adoulii <omaradouli79@gmail.com>
2024-02-20 12:19:09 +0100

    Connect to MongoDB Atlas

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 // BASE SETUP
 // ==============================================
-var express = require('express');
-var app     = express();
-var port    =   process.env.PORT || 3000;
-var { mongoose } = require("./dbConfig/mongoose");
+var express = require("express");
+var app = express();
+var { connect } = require("./config/mongoose");
 var bodyParser = require("body-parser");
-const swaggerDoc = require('./docs/swaggerDoc')
+const swaggerDoc = require("./docs/swaggerDoc");
+const { port, env } = require("./config/vars");
 
-const userRoutes = require('./routes/userRoutes');
+const userRoutes = require("./routes/userRoutes");
 // ==============================================
 app.use(bodyParser.json());
 swaggerDoc(app);
 
-app.use('/api', userRoutes);
+connect();
+
+app.use("/api", userRoutes);
 // ==============================================
 // START THE SERVER
 // ==============================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log("Magic happens on port " + port);
---
+++ config/mongoose.js
@@ -0,0 +1,22 @@
+var mongoose = require("mongoose");
+const logger = require("../config/logger");
+const { mongo } = require("../config/vars");
+const f = require("util").format;
+mongoose.Promise = global.Promise;
+const url = f(
+  `mongodb+srv://${mongo.user}:${mongo.pass}@${mongo.uri}/?retryWrites=true&w=majority`
+);
+const connect = () => {
+  mongoose.connect(url, {});
+  return mongoose.connection;
+};
+
+mongoose.connection.on("connected", () => {
+  logger.info(`connected to database `);
+});
+
+mongoose.connection.on("error", (err) => {
+  logger.error(`MongoDB connection error: ${err}`);
+});
+
+module.exports = { connect };
