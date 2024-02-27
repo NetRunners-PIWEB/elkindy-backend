4d28c563141257ae9ea85c38c4ea30dac519ef80
Imen-Frigui <imen_frigui@outlook.com>
2024-02-27 14:22:33 +0100

    EK-6 <fetch teacher>

--- models/user.js
+++ models/user.js
@@ -2,40 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-<<<<<<< HEAD
-    username: String, 
-    firstName: String, 
-    lastName: String, 
-    age: Number,
-    email: String, 
-    password: String, 
-    registrationDate: Date,
-    endDate: Date,
-    dateOfBirth: Date, 
-    phoneNumber: String, 
-    gender: String, 
-    address: String, 
-    image: String,
-    createdAt: Date, 
-    updatedAt: Date, 
-    status: {
-        type: String,
-        enum:["active", "inactive","deleted","suspended"],
-      },
 
-      isDeleted: {
-        type: Boolean,
-        default: false,
-      },
-    role: {
-        type: Schema.ObjectId,
-        ref:'Roles'
-      },
-   
-});
-
-module.exports = mongoose.model("Users", userSchema);
-=======
   username: {
     type: String,
     unique: true,
@@ -100,4 +67,3 @@ userSchema.statics.register = async (email, password) => {
   }
 }
 module.exports = mongoose.model("Users", userSchema);
->>>>>>> 1d2ec6cb46d73e3518cdb4619a40198741863bd7
