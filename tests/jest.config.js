fba9b458dada4f4cc033dc235615ed9fac70b23b
Adoulii <omaradouli79@gmail.com>
2024-03-14 23:44:57 +0100

    add user instruments

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -3,8 +3,8 @@ module.exports = {
   testEnvironment: "node",
   moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"],
 
-  // testRegex: "(/__tests__/.*|(\\.|/)(test|spec|instrument))\\.[jt]sx?$",
-  testRegex: "/tests/instrument\\.test\\.[jt]sx?$",
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec|instrument|exam))\\.[jt]sx?$",
+  // testRegex: "/tests/instrument\\.test\\.[jt]sx?$",
   collectCoverage: true,
   coverageDirectory: "<rootDir>/coverage",
   coverageReporters: ["lcov", "text-summary"],
