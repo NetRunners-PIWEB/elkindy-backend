4bd9821302b76da28e22ef224920b9a6614751f5
hanin303 <benjemaahanin@gmail.com>
2024-03-22 10:11:56 +0100

    adding_secret_key

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -4,8 +4,8 @@ module.exports = {
   moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"],
 
   // testRegex: "(/__tests__/.*|(\\.|/)(test|spec|instrument))\\.[jt]sx?$",
-  testRegex: "/tests/instrument\\.test\\.[jt]sx?$",
-  collectCoverage: true,
+  //testRegex: "/tests/instrument\\.test\\.[jt]sx?$",
+  //testRegex: "(/__tests__/.*|(\\.|/)(test|spec|instrument|exam|event))\\.[jt]sx?$",  collectCoverage: true,
   coverageDirectory: "<rootDir>/coverage",
   coverageReporters: ["lcov", "text-summary"],
 };
