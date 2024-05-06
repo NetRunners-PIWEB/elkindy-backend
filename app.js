eb0ba3fe72b5814893094cb1955595defee5f053
Imen-Frigui <imen_frigui@outlook.com>
2024-05-06 09:22:37 +0100

    FinalVersion

--- app.js
+++ app.js
@@ -67,7 +67,6 @@ app.use(function (req, res, next) {
 
 app.use(
   cors({
-    origin: ["http://localhost:3001", "http://192.168.167.23:3001","https://elkindy-frontend.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
